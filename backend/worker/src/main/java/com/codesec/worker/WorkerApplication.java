0fd529749fd87181c3ba2475891ea3de1ce9530c
foo <foo@bar.com>
2026-07-01 14:27:44 +0800

    E-S4-DB: fix WorkerApplication scanBasePackages to discover api module beans

--- backend/worker/src/main/java/com/codesec/worker/WorkerApplication.java
+++ backend/worker/src/main/java/com/codesec/worker/WorkerApplication.java
@@ -3,7 +3,7 @@ package com.codesec.worker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.codesec.worker", "com.codesec.api", "com.codesec.common"})
 public class WorkerApplication {
     public static void main(String[] args) {
         SpringApplication.run(WorkerApplication.class, args);
