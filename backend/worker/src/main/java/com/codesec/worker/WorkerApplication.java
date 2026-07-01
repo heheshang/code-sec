8916a7702dfc41119b34c406dc82a52063945169
foo <foo@bar.com>
2026-07-01 14:29:36 +0800

    E-S4-DB: add engineadapter to WorkerApplication scanBasePackages

--- backend/worker/src/main/java/com/codesec/worker/WorkerApplication.java
+++ backend/worker/src/main/java/com/codesec/worker/WorkerApplication.java
@@ -3,7 +3,7 @@ package com.codesec.worker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {"com.codesec.worker", "com.codesec.api", "com.codesec.common"})
+@SpringBootApplication(scanBasePackages = {"com.codesec.worker", "com.codesec.api", "com.codesec.common", "com.codesec.engineadapter"})
 public class WorkerApplication {
     public static void main(String[] args) {
         SpringApplication.run(WorkerApplication.class, args);
